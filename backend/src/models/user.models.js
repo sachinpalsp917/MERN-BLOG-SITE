482727c174e9c6e878fa50bd79a89458586ad673
sachinpalsp917 <sachinpalsp917@gmail.com>
2024-03-04 22:15:24 +0530

    updated user model

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -20,4 +20,6 @@ const UserSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-export default User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+export default User;
