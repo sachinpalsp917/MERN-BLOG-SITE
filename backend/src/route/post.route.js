5308b86f917c3114742c890af7d742cad6917beb
sachinpalsp917 <sachinpalsp917@gmail.com>
2024-04-02 11:47:55 +0530

    hoola

--- backend/src/route/post.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {verifyUser} from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
-
-const router = express.Router();
-
-router.post('/create', verifyUser, create);
-
-export default router;
